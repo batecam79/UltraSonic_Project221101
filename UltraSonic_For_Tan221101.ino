462b420b703481a7cfbf3515ccea0ad015f4bbb4
batecam79 <61323776+batecam79@users.noreply.github.com>
2022-12-04 13:47:37 +0800

    Update UltraSonic_For_Tan221101.ino

--- UltraSonic_For_Tan221101.ino
+++ UltraSonic_For_Tan221101.ino
@@ -1,20 +1,22 @@
 #include <SoftwareSerial.h>
-//#include <NeoSWSerial.h>
+//#include <millisDelay.h>
+#include <MsTimer2.h>     //timer lib
 
-//relay_Pin setting --------------------------------------------
-
-const int relay_1 = 6;
-const int relay_2 = 7;
-const int relay_3 = 8;
+const int relay_1 = 6;    //relay 1 Pin
+const int relay_2 = 7;    //relay 2 Pin
+const int relay_3 = 8;    //relay 3 Pin
 
 //sensor_1 setting --------------------------------------------
+const byte tx1Pin = 10;    
+const byte rx1Pin = 11;
+
+SoftwareSerial sensor_1 (rx1Pin, tx1Pin);
 
 unsigned int S1Head = 0;
 unsigned int S1MSByteDist = 0;
 unsigned int S1LSByteDist = 0;
 unsigned int S1checksum = 0;
 unsigned int S1_Dist = 0;
-
 
 //sensor_2 setting --------------------------------------------
 const byte tx2Pin = 2;
@@ -28,7 +30,6 @@ unsigned int S2LSByteDist = 0;
 unsigned int S2checksum = 0;
 unsigned int S2_Dist = 0;
 
-
 //sensor_3 setting --------------------------------------------
 const byte tx3Pin = 4;
 const byte rx3Pin = 5;
@@ -41,133 +42,187 @@ unsigned int S3LSByteDist = 0;
 unsigned int S3checksum = 0;
 unsigned int S3_Dist = 0;
 
-//-----------------timer-------------------------------
 
-unsigned long S1_pS = 0;    //sensor1 previousMillis
-unsigned long S1_cS = 0;    //sensor1 currentMillis
+int Current_Serial_Job = 1;	
+	//0:Waiting
+	//1:Send Data From Serail 1
+	//2:Read Data From Serail 1
+	//3:Send Data From Serail 2
+	//4:Read Data From Serail 2
+	//5:Send Data From Serail 3
+	//6:Read Data From Serail 3
 
-unsigned long S2_pS = 0;    //sensor1 previousMillis
-unsigned long S2_cS = 0;    //sensor1 currentMillis
+int Current_Relay1_Job = 0;
+	//0:Do Nothing
+	//1:Close Relay
+	//2:Delay 10 Second
+int Current_Relay1_Count = 0;
 
-unsigned long S3_pS = 0;    //sensor1 previousMillis
-unsigned long S3_cS = 0;    //sensor1 currentMillis
+int Current_Relay2_Job = 0;
+	//0:Do Nothing
+	//1:Close Relay
+	//2:Delay 10 Second
+int Current_Relay2_Count = 0;
 
-const long Relay1_Stopinterval = 10000;      //此时间为上次继电器针脚LOW之后的间隔时间，再这个间隔时间后才会继续响应digitalWrite
-const long Relay2_Stopinterval = 10000;
-const long Relay3_Stopinterval = 10000;
+int Current_Relay3_Job = 0;
+	//0:Do Nothing
+	//1:Close Relay
+	//2:Delay 10 Second
+int Current_Relay3_Count = 0;
 
-const long Relay1_Oninterval = 200;
-const long Relay2_Oninterval = 200;
-const long Relay3_Oninterval = 200;
+//中断服务程序
+void onTimer()
+{
+	switch (Current_Serial_Job){
+		case 1:
+			sensor_1.write(0x55);    //sensor trigger byte
+      sensor_1.listen();       //open softserial and wait for incoming data
+			break;
 
-int relay1_FirstOn = 0;
-int relay2_FirstOn = 0;
-int relay3_FirstOn = 0;
+		case 2:
+			if (sensor_1.available() >= 4){	//Check Serail 1
+				S1Head = sensor_1.read();
+        S1MSByteDist = sensor_1.read(); // 
+        S1LSByteDist  = sensor_1.read();
+        S1checksum  = sensor_1.read();
+        S1_Dist  = S1MSByteDist * 256 + S1LSByteDist; // read Distance
+        Serial.print("Distance-1: ");
+        Serial.print(S1_Dist, DEC);
+        Serial.print(" mm    ");
+        Serial.println();
+				//Read Serial 1 Data and Calculate Distance
+				if ((S1_Dist>1000) && (S1_Dist<2000)){	//Check if the distance is in expected range
+					Current_Relay1_Job=1;
+					Current_Relay1_Count=0;
+			        digitalWrite(relay_1, HIGH);	//Open Relay
+				}
+			}
+			break;
 
-bool relay1State = LOW;
-bool relay2State = LOW;
-bool relay3State = LOW;
+		case 3:
+			sensor_2.write(0x55);
+      sensor_2.listen();
+			break;
+		case 4:
 
+			if (sensor_2.available() >= 4){	//Check Serail 2
+				S2Head = sensor_2.read();
+        S2MSByteDist = sensor_2.read(); // 
+        S2LSByteDist  = sensor_2.read();
+        S2checksum  = sensor_2.read();
+        S2_Dist  = S2MSByteDist * 256 + S2LSByteDist; // Distance
+        Serial.print("Distance-2: ");
+        Serial.print(S2_Dist, DEC);
+        Serial.print(" mm    ");
+        Serial.println();
+				//Read Serial 2 Data and Calculate Distance
+				if ((S2_Dist>1000) && (S2_Dist<2000)){	//Check if the distance is in expected range
+					Current_Relay2_Job=1;
+					Current_Relay2_Count=0;
+			        digitalWrite(relay_2, HIGH);	//Open Relay
+				}
+			}
+			break;
+		case 5:
+			sensor_3.write(0x55);
+      sensor_3.listen();
+			break;
+		case 6:
 
-//---------------------------setup---------------------------------
-void setup()  {
+			if (sensor_3.available() >= 4){	//Check Serail 3
+				S3Head = sensor_3.read();
+        S3MSByteDist = sensor_3.read(); // 
+        S3LSByteDist = sensor_3.read();
+        S3checksum = sensor_3.read();
+        S3_Dist  = S3MSByteDist * 256 + S3LSByteDist; // Distance
+        Serial.print("Distance-3: ");
+        Serial.print(S3_Dist, DEC);
+        Serial.print(" mm    ");
+        Serial.println();
+				//Read Serial 3 Data and Calculate Distance
+				if ((S3_Dist>1000) && (S3_Dist<2000)){	//Check if the distance is in expected range
+					Current_Relay3_Job=1;
+					Current_Relay3_Count=0;
+			        digitalWrite(relay_3, HIGH);	//Open Relay
+				}
+			}
+			break;
+	}
 
-    // Define pin modes for TX and RX
-    //0,1 hardwareSerial
-    pinMode(rx2Pin, INPUT);
-    pinMode(tx2Pin, OUTPUT);
-    pinMode(rx3Pin, INPUT);
-    pinMode(tx3Pin, OUTPUT);
-    pinMode(relay_1, OUTPUT);
-    pinMode(relay_2, OUTPUT);
-    pinMode(relay_3, OUTPUT);
+	if ((Current_Relay1_Job > 0) && (Current_Relay2_Job > 0) && (Current_Relay3_Job > 0)){		//Three Relay all is Opened Or Waiting
+		Current_Serial_Job = 0;		//Waiting one Serial can Send Data
+  }
+  else{
+    if (++Current_Serial_Job > 6)				//Next Step>6 go to Serial1 Send Data
+      Current_Serial_Job=1;
+    if ((Current_Serial_Job == 1) && (Current_Relay1_Job>0))		//Next Step is Serial 2 Send Data ,but Relay 2 is Opened Or Waiting
+      Current_Serial_Job=3;
+    if ((Current_Serial_Job == 3) && (Current_Relay2_Job>0))		//Next Step is Serial 2 Send Data ,but Relay 2 is Opened Or Waiting
+      Current_Serial_Job=5;
+    if ((Current_Serial_Job == 5) && (Current_Relay3_Job>0))		//Next Step is Serial 3 Send Data ,but Relay 3 is Opened Or Waiting
+      Current_Serial_Job=1;
+  }
 
-    // Set the baud rate for the SoftwareSerial object
-    Serial.begin(9600);
-    sensor_2.begin(9600);
-    sensor_3.begin(9600);
+	if (Current_Relay1_Job > 0){
+		Current_Relay1_Count++;
+		if ((Current_Relay1_Job == 1) && (Current_Relay1_Count >= 20)){		//relay open for 1200ms(60msx20=1200ms)
+	        digitalWrite(relay_1, LOW);
+			Current_Relay1_Job = 2;
+			Current_Relay1_Count = 0;
+		}
+		else if ((Current_Relay1_Job == 2) && (Current_Relay1_Count >= 200)){		//sensor stop for 12s(60msx200=12000ms)
+			Current_Relay1_Job = 0;
+			Current_Relay1_Count = 0;
+		}
+	}
 
-    digitalWrite(relay_1, relay1State);
-    digitalWrite(relay_2, relay2State);
-    digitalWrite(relay_3, relay3State);
+	if (Current_Relay2_Job > 0){
+		Current_Relay2_Count++;
+		if ((Current_Relay2_Job == 1) && (Current_Relay2_Count >= 20)){		//relay open for 1200ms(60msx20=1200ms)
+	        digitalWrite(relay_2, LOW);
+			Current_Relay2_Job = 2;
+			Current_Relay2_Count = 0;
+		}
+		else if ((Current_Relay2_Job == 2) && (Current_Relay2_Count >= 200)){		//sensor stop for 12s(60msx200=12000ms)
+			Current_Relay2_Job = 0;
+			Current_Relay2_Count = 0;
+		}
+	}
 
-
+	if (Current_Relay3_Job > 0){
+		Current_Relay3_Count++;
+		if ((Current_Relay3_Job == 1) && (Current_Relay3_Count >= 20)){		//relay open for 1200ms(60msx20=1200ms)
+	        digitalWrite(relay_3, LOW);
+			Current_Relay3_Job = 2;
+			Current_Relay3_Count = 0;
+		}
+		else if ((Current_Relay3_Job == 2) && (Current_Relay3_Count >= 200)){		//sensor stop for 12s(60msx200=12000ms)
+			Current_Relay3_Job = 0;
+			Current_Relay3_Count = 0;
+		}
+	}
 }
 
-void loop() {
+void setup()
+{
+  pinMode(rx2Pin, INPUT);
+  pinMode(tx2Pin, OUTPUT);
+  pinMode(rx3Pin, INPUT);
+  pinMode(tx3Pin, OUTPUT);
+  pinMode(relay_1, OUTPUT);
+  pinMode(relay_2, OUTPUT);
+  pinMode(relay_3, OUTPUT);
 
-  Serial.write(0x55);
-  delay(60);
-  while(Serial.available() >= 4) //// Check reception of 4 bytes
-  { 
-    S1Head = Serial.read();
-    S1MSByteDist = Serial.read(); // 
-    S1LSByteDist  = Serial.read();
-    S1checksum  = Serial.read();
-    S1_Dist  = S1MSByteDist * 256 + S1LSByteDist; // Distance
-    // Serial.print("Distance-1: ");
-    // Serial.print(S1_Dist, DEC);
-    // Serial.print(" mm    ");
-    // Serial.println();
-    if((S1_Dist > 1000) && (S1_Dist < 2000) && (relay1State == LOW)) // Check if the distance is in expected range
-    {   
-      if(relay1_FirstOn == 0){      //turn on relay1 for 10sec and set relay1_FirstON = 1
-        digitalWrite(relay_1, HIGH);
-        relay1_FirstOn = 1;
-        relay1State = HIGH;
-        S1_pS = millis();
-      }
-    }
-
-  }
-
-
-
-  sensor_2.write(0x55);
-  sensor_2.listen();
-  delay(60);
-  while(sensor_2.available() >= 4) // Check reception of 4 bytes
-  {
-    S2Head = sensor_2.read();
-    S2MSByteDist = sensor_2.read(); // 
-    S2LSByteDist  = sensor_2.read();
-    S2checksum  = sensor_2.read();
-    S2_Dist  = S2MSByteDist * 256 + S2LSByteDist; // Distance
-    // Serial.print("Distance-2: ");
-    // Serial.print(S2_Dist, DEC);
-    // Serial.print(" mm    ");
-    // Serial.println();
-    
-    if((S2_Dist > 1000) && (S2_Dist < 2000)) // Check if the distance is in expected range
-    {
-      digitalWrite(relay_2, HIGH);
-    }
-    else{
-      digitalWrite(relay_2, LOW);
-    }
-  }  
   
-  sensor_3.write(0x55);
-  sensor_3.listen();
-  delay(60);
-  while(sensor_3.available() >= 4) // Check reception of 4 bytes
-  {
-    S3Head = sensor_3.read();
-    S3MSByteDist = sensor_3.read(); // 
-    S3LSByteDist  = sensor_3.read();
-    S3checksum  = sensor_3.read();
-    S3_Dist  = S3MSByteDist * 256 + S3LSByteDist; // Distance
-    // Serial.print("Distance-3: ");
-    // Serial.print(S3_Dist, DEC);
-    // Serial.print(" mm    ");
-    // Serial.println();
-    if((S3_Dist > 1000) && (S3_Dist < 2000)) // Check if the distance is in expected range
-    {
-      digitalWrite(relay_3, HIGH);
-    }
-    else{
-      digitalWrite(relay_3, LOW);
-      }
-  }
+  Serial.begin(9600);       //initialize Serial for debug
+  sensor_1.begin(9600);     //initialize Serial for sensor1
+  sensor_2.begin(9600);     //initialize Serial for sensor2
+  sensor_3.begin(9600);     //initialize Serial for sensor3
+ 
+  MsTimer2::set(60, onTimer); //Set Timer=60ms，after 60ms onTimer_Serial1(); The Timer must>=60ms for sensor reading.
+  MsTimer2::start(); //Start Timer
+}
+
+void loop(){
 
 }
